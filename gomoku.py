5a289b8ac5d1a4530c9b26d264d3029ad5a0c111
Qin <aries.duanmu@gmail.com>
2017-07-07 23:56:43 +0800

    prettier

--- gomoku.py
+++ gomoku.py
@@ -3,23 +3,24 @@ from pygame.locals import *
 
 # Define some colors
 BLACK  = (0, 0, 0)
-WHITE  = (255, 255, 255)
+WHITE  = (245, 245, 245)
 RED    = (255, 0, 0)
-YELLOW = (255, 222, 173)
+YELLOW = (205, 164, 131)
 GREEN  = (0, 255, 0)
+BACKGROUND = (64,41,22)
 
 PLAYER = False
 
 # Define grid globals
-HEIGHT = 20
 WIDTH = 20
-MARGIN = 5
+MARGIN = 1
+PADDING = 20
  
 class App:
     def __init__(self):
         self._running = True
         self._display_surf = None
-        self.size = self.weight, self.height = 380, 380
+        self.size = self.weight, self.height = (WIDTH + MARGIN) * 15 + MARGIN + PADDING * 2, (WIDTH + MARGIN) * 15 + MARGIN + PADDING * 2
         self.grid = [[0 for x in range(15)] for y in range(15)]
 
  
@@ -27,18 +28,25 @@ class App:
         pygame.init()
         pygame.font.init()
         self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
-        self._display_surf.fill(RED) 
+        self._display_surf.fill(YELLOW) 
         pygame.display.set_caption('Gomoku')
         
+        # Draw background rect for game area
+
+        pygame.draw.rect(self._display_surf, BACKGROUND,
+                         [PADDING,
+                          PADDING,
+                          (WIDTH + MARGIN) * 15 + MARGIN,
+                          (WIDTH + MARGIN) * 15 + MARGIN]) 
 
         # Draw the grid
         for row in range(15):
             for column in range(15):
                 color = YELLOW
                 pygame.draw.rect(self._display_surf, color,
-                                 [(MARGIN + HEIGHT) * column + MARGIN,
-                                  (MARGIN + WIDTH) * row + MARGIN,
-                                  HEIGHT,
+                                 [(MARGIN + WIDTH) * column + MARGIN + PADDING,
+                                  (MARGIN + WIDTH) * row + MARGIN + PADDING,
+                                  WIDTH,
                                   WIDTH])
                 pygame.display.update()
         
@@ -48,51 +56,54 @@ class App:
     def on_event(self, event):
         if event.type == pygame.QUIT:
             self._running = False
-            
+        
+
         if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
             
-            c = pos[0] // (20+5)
-            r = pos[1] // (20+5)
+            c = (pos[0] - PADDING) // (WIDTH+MARGIN)
+            r = (pos[1] - PADDING) // (WIDTH+MARGIN)
             
-            global PLAYER
-            
-            if self.grid[c][r] == 0:
-                self.grid[c][r] = 1
+            if 0 <= c < 15 and 0 <= r < 15:
+                global PLAYER
                 
-                if PLAYER:
-                    color = WHITE
+                if self.grid[c][r] == 0:
                     self.grid[c][r] = 1
-                else:
-                    color = BLACK
-                    self.grid[c][r] = 2
                     
-                pygame.draw.rect(self._display_surf, color,
-                                 [(MARGIN + HEIGHT) * c + MARGIN,
-                                  (MARGIN + WIDTH) * r + MARGIN,
-                                  HEIGHT,
-                                  WIDTH])
-                pygame.display.update()
-                
-                if self.check_win([c,r],PLAYER):
-                    win = "Player 1 win" if PLAYER else "Player 2 win"
-                    myfont = pygame.font.SysFont('Comic Sans MS', 30)
-                    text_surface = myfont.render(win, False, (0, 0, 0))
-                    self._display_surf.blit(text_surface, (0,0))
+                    if PLAYER:
+                        color = WHITE
+                        self.grid[c][r] = 1
+                    else:
+                        color = BLACK
+                        self.grid[c][r] = 2
+                        
+                    pygame.draw.circle(self._display_surf, color,
+                                     ((MARGIN + WIDTH) * c + MARGIN + WIDTH / 2 + PADDING,
+                                      (MARGIN + WIDTH) * r + MARGIN + WIDTH / 2 + PADDING),
+                                      WIDTH / 2 - MARGIN,0)
+                    
+                    
+                    # check win
+                    if self.check_win([c,r],PLAYER):
+                        win = "Player 1 win" if PLAYER else "Player 2 win"
+                        myfont = pygame.font.SysFont('Comic Sans MS', 30)
+                        text_surface = myfont.render(win, False, (0, 0, 0))
+                        self._display_surf.blit(text_surface, (0,0))
 
-                    # Incomplete button functionalit for new_game
-                    pygame.draw.rect(self._display_surf, GREEN,(50,150,100,50))
-                    pygame.draw.rect(self._display_surf, RED,(250,150,100,50))
-                    pygame.display.update()
+                        # Incomplete button functionalit for new_game
+                        pygame.draw.rect(self._display_surf, GREEN,(50,150,100,50))
+                        pygame.draw.rect(self._display_surf, RED,(250,150,100,50))
+                        
 
-                PLAYER = not PLAYER
+                    PLAYER = not PLAYER
             
     def on_loop(self):
         pass
 
     
     def on_render(self):
-        pass
+        pygame.display.update()
+        
 
     
     def on_cleanup(self):
