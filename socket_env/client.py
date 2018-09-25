a37874884bbe6c9dcdcfe30aa36add9d11a6f078
ariesduanmu <aries.duanmu@gmail.com>
2018-09-25 14:59:41 +0800

    update socket server to python3.x

--- socket_env/client.py
+++ socket_env/client.py
@@ -1,17 +1,45 @@
 import socket
-target_host = "0.0.0.0"
-target_port = 9999
+import argparse
+import re
+import sys
 
-# Create
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Connect
-client.connect((target_host,target_port))
+def parse_options():
+    parser = argparse.ArgumentParser(usage='%(prog)s [options]',
+                                     description='Gomoku socket client @Ludisposed & @Qin',
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog=
+'''
+Examples:
+python client.py -h '0.0.0.0' -p 9999
+'''
+                                        )
+    parser.add_argument('-o','--host', type=str, default="0.0.0.0", help='server host')
+    parser.add_argument('-p','--port', type=int, default=9999, help='server port')
+    args = parser.parse_args()
 
-while True:
-    # Send
-    sendstring = raw_input("What do we want to send>>> ")
-    client.send(sendstring)
-    # Recieve
-    response = client.recv(4096)
-    
-    print response 
+
+    host_pattern = "((?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:(?<!\.)|\.)){4}"
+    host = re.match(host_pattern, args.host)
+    valid_host = (host and host.group(0) == args.host)
+
+    if not (args.host == "localhost" or valid_host):
+        print("[-] IPV4 host is not valid")
+        sys.exit(1)
+    return args
+
+def main(host, port):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host,port))
+
+    while True:
+        # Send
+        sendstring = input("What do we want to send>>> ")
+        client.send(sendstring.encode("utf-8"))
+        # Recieve
+        response = client.recv(4096)
+        
+        print(response.decode("utf-8"))
+
+if __name__ == "__main__":
+    args = parse_options()
+    main(args.host,args.port) 
--- socket_env/server.py
+++ socket_env/server.py
@@ -1,37 +1,64 @@
 import socket
 import threading
+import argparse
+import re
+import sys
 
-bind_ip = "0.0.0.0"
-bind_port = 9999
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def parse_options():
+    parser = argparse.ArgumentParser(usage='%(prog)s [options]',
+                                     description='Gomoku socket server @Ludisposed & @Qin',
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog=
+'''
+Examples:
+python client.py -h '0.0.0.0' -p 9999
+'''
+                                        )
+    parser.add_argument('-i','--ip', type=str, default="0.0.0.0", help='server host')
+    parser.add_argument('-p','--port', type=int, default=9999, help='server port')
+    args = parser.parse_args()
 
-server.bind((bind_ip,bind_port))
 
-server.listen(5)
+    ip_pattern = "((?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:(?<!\.)|\.)){4}"
+    ip = re.match(ip_pattern, args.ip)
+    valid_ip = (ip and ip.group(0) == args.ip)
 
-print "[*] Listening on %s:%d" % (bind_ip, bind_port)
+    if not (args.ip == "localhost" or valid_ip):
+        print("[-] IPV4 host is not valid")
+        sys.exit(1)
+    return args
 
 def handle_client(client_socket):
     # print what client says
     request = client_socket.recv(1024)
 
-    print "[*] Recieved %s" % request
+    print(f"[*] Recieved: {request.decode('utf-8')}")
 
     if(request=="new game"):
         client_socket.send("let's start a new game")
     # send back a packet
-    client_socket.send("\n\nOK!")
+    client_socket.send(b"\n\nOK!")
 
     client_socket.close()
 
-while True:
-    try:
-        client, addr = server.accept()
+def main(ip, port):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((ip, port))
+    server.listen(5)
+    print(f"[*] Listening on {ip}:{port}")
 
-        print "[*] Accepted connection from: %s:%d" % (addr[0], addr[1])
+    while True:
+        try:
+            client, addr = server.accept()
 
-        # spin the client thread to handle incoming data
-        client_handler = threading.Thread(target=handle_client, args=(client,))
-        client_handler.start()
-    except KeyboardInterrupt:
-        server.close()
+            print(f"[*] Accepted connection from: {addr[0]}:{addr[1]}")
+
+            # spin the client thread to handle incoming data
+            client_handler = threading.Thread(target=handle_client, args=(client,))
+            client_handler.start()
+        except KeyboardInterrupt:
+            server.close()
+
+if __name__ == "__main__":
+    args = parse_options()
+    main(args.ip, args.port)
